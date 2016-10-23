2f7f104c29e6425b8f9b033cba7e90f35d8131e4
akosgarai <szlem01@gmail.com>
2016-10-23 21:01:39 +0200

    add some component and test from other project

--- app.js
+++ app.js
@@ -1,2 +1,152 @@
 function Graph (clusterF, clusterL, weightMatrix){
+    /*
+     * F cluster - array of strings, represents the name of elements in F
+     * */
+    if (typeof(clusterF) != 'undefined') {
+        this.F = clusterF;
+    } else {
+        this.F = [];
+    }
+    /*
+     * L cluster - same as F
+     * */
+    if (typeof(clusterL) != 'undefined') {
+        this.L = clusterL;
+    } else {
+        this.L = [];
+    }
+    /*
+     * weightMatrix
+     * */
+    if (typeof(weightMatrix) != "undefined") {
+        this.weightMatrix = weightMatrix;
+    } else {
+        this.weightMatrix = [[]];
+    }
+}
+
+function Egervary (graph) {
+    /*
+     * init graph for algorithm
+     * */
+    if (typeof graph == 'undefined') {
+        this.G = new Graph();
+    } else {
+        this.G = graph;
+    }
+
+    /*
+     * initMatchedM function
+     * 0.th step of algo. setup matched object
+     * matchedM[i] = j -> if j == -1 : F[i] is unpaired, if j != -1 : F[i]'s pair is L[j]
+     * */
+    this.initMatchedM = function () {
+        this.matchedM = [];
+        for (var i in this.G['F']) {
+            this.matchedM.push(-1);
+        }
+    };
+    /*
+     * createPair function
+     * input : iF index from clusterF, iL index from clusterL
+     * it sets up matchedM[iF] to iL (matchedM[iF] = iL)
+     * */
+    this.createPair = function (iF, iL) {
+        this.matchedM[iF] = iL;
+    };
+    /*
+     * findFirstSingle function
+     * single : ID of element from clusterL or negative number (-1)
+     * return: the fist index that also appears in the unmatched object, or -1
+     * */
+    this.findFirstSingle = function () {
+        for (var i in this.matchedM) {
+            if (this.matchedM[i] == -1) {
+                return i;
+            }
+        }
+        return '-1';
+    };
+
+    /*
+     * initLabels function
+     * 0.th step of algo. setup matched object
+     * labels[i] = j -> if i is from cluster L : j = 0, if i is from cluster F : j = max weight from i
+     * */
+    this.initLabels = function () {
+        this.labels = {};
+        for (var i in this.G['L']) {
+            this.labels[this.G['L'][i]] = 0;
+        }
+        for (var i in this.G['F']) {
+            var weight = 0;
+            for (var j in this.G['weightMatrix'][i]) {
+                if (this.G['weightMatrix'][i][j] > weight) {
+                    weight = this.G['weightMatrix'][i][j];
+                }
+            }
+            this.labels[this.G['F'][i]] = weight;
+        }
+    };
+    /*
+     * stepZero function
+     * it makes the 0. step. calls initMatchedM, initLables and initRedEdges functions
+     * */
+    this.stepZero = function () {
+        this.initMatchedM();
+        this.initLabels();
+        this.initRedEdges();
+    };
+    /*
+     * isRedEdge function
+     * input : iF - index of element from clusterF, iL - index of element from clusterL
+     * output : bool
+     * */
+    this.isRedEdge = function (iF, iL) {
+        var weight = this.G['weightMatrix'][iF][iL];
+        var sumLabels = this.labels[this.G['F'][iF]] + this.labels[this.G['L'][iL]];
+
+        if (weight == sumLabels) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+    /*
+     * initRedEdges function
+     * helper function to get the first red edges after step 0
+     * */
+    this.initRedEdges = function () {
+        this.redEdges = [];
+        for (var i in this.G['weightMatrix']) {
+            var mRow = this.G['weightMatrix'][i];
+            var row = [];
+            for (var j in mRow) {
+                if (this.isRedEdge(i, j)) {
+                    row.push(mRow[j]);
+                } else {
+                    row.push(0)
+                }
+            }
+            this.redEdges.push(row);
+        }
+    };
+    /*
+     * isMatchedMFull function
+     * return false if matchedM[i] == -1
+     * */
+    this.isMatchedMFull = function () {
+        for (var i in this.matchedM) {
+            if (this.matchedM[i] == -1) {
+                return false;
+            }
+        }
+        return true;
+    };
+    /*
+     * stepOne function
+     * it makes the 1. step.
+     * */
+    this.stepOne = function () {
+    };
 }
--- tests/app-test.js
+++ tests/app-test.js
@@ -3,3 +3,253 @@ describe('example test', function() {
         expect('foo').toBe('foo');
     });
 });
+
+describe('Graph constructor tests', function () {
+    it('Create graph without parameters', function () {
+        var G = new Graph();
+        var expected = {'F' : [], 'L' : [], 'weightMatrix' : [[]]};
+        expect({'F' : G.F, 'L' : G.L, 'weightMatrix' : G.weightMatrix}).toEqual(expected);
+    });
+    it('Create graph with clusterF param', function () {
+        var G = new Graph([1,2]);
+        var expected = {'F' : [1,2], 'L' : [], 'weightMatrix' : [[]]};
+        expect({'F' : G.F, 'L' : G.L, 'weightMatrix' : G.weightMatrix}).toEqual(expected);
+    });
+    it('Create graph with clusterF, clusterL param', function () {
+        var G = new Graph([1,2],[3,4]);
+        var expected = {'F' : [1,2], 'L' : [3,4], 'weightMatrix' : [[]]};
+        expect({'F' : G.F, 'L' : G.L, 'weightMatrix' : G.weightMatrix}).toEqual(expected);
+    });
+    it('Create graph with clusterF, clusterL, weightMatrix param', function () {
+        var G = new Graph([1,2],[3,4], [[1,1],[1,0]]);
+        var expected = {'F' : [1,2], 'L' : [3,4], 'weightMatrix' : [[1,1],[1,0]]};
+        expect({'F' : G.F, 'L' : G.L, 'weightMatrix' : G.weightMatrix}).toEqual(expected);
+    });
+});
+
+describe('Egervary related tests', function () {
+    describe('Egervary constructor tests', function () {
+        it('Egervary without parameters', function () {
+            var expected = new Graph();
+            var E = new Egervary();
+            expect(E.G).toEqual(expected);
+        });
+        it('Egervary with parameters', function () {
+            var expected = new Graph([1,2],[3,4], [[1,1],[1,0]]);
+            var E = new Egervary(expected);
+            expect(E.G).toEqual(expected);
+        });
+    });
+    describe('Egervary initMatchedM function test', function () {
+        it('Egervary initMatchedM without input graph', function () {
+            var expected = [];
+            var E = new Egervary();
+            E.initMatchedM();
+            expect(E.matchedM).toEqual(expected);
+        });
+        it('Egervary initMatchedM with input graph', function () {
+            var expected = [-1, -1];
+            var G = new Graph([1,2],[3,4], [[1,1],[1,0]]);
+            var E = new Egervary(G);
+            E.initMatchedM();
+            expect(E.matchedM).toEqual(expected);
+        });
+    });
+    describe('Egervary initLabels function test', function () {
+        it('Egervary initLabels without input graph', function () {
+            var expected = {};
+            var E = new Egervary();
+            E.initLabels();
+            expect(E.labels).toEqual(expected);
+        });
+        it('Egervary initLabels with input graph testset 1', function () {
+            var expected = {1:1,2:1,3:0,4:0};
+            var G = new Graph([1,2],[3,4], [[1,1],[1,0]]);
+            var E = new Egervary(G);
+            E.initLabels();
+            expect(E.labels).toEqual(expected);
+        });
+        it('Egervary initLabels with input graph testset 2', function () {
+            var expected = {1:2,2:3,3:0,4:0};
+            var G = new Graph([1,2],[3,4], [[2,1],[3,0]]);
+            var E = new Egervary(G);
+            E.initLabels();
+            expect(E.labels).toEqual(expected);
+        });
+        it('Egervary initLabels with input graph testset 3', function () {
+            var expected = {'ken':2,2:3,3:0,'mary':0};
+            var G = new Graph(['ken',2],[3,'mary'], [[2,1],[3,0]]);
+            var E = new Egervary(G);
+            E.initLabels();
+            expect(E.labels).toEqual(expected);
+        });
+    });
+    describe('Egervary stepZero function test', function () {
+        it('Egervary stepZero without input graph', function () {
+            var expected = {'matchedM':[], 'labels':{}};
+            var E = new Egervary();
+            E.stepZero();
+            expect({'matchedM':E.matchedM, 'labels':E.labels}).toEqual(expected);
+        });
+        it('Egervary stepZero with input graph testset 1', function () {
+            var expected = {'matchedM':[-1,-1], 'labels':{1:1,2:1,3:0,4:0}};
+            var G = new Graph([1,2],[3,4], [[1,1],[1,0]]);
+            var E = new Egervary(G);
+            E.stepZero();
+            expect({'matchedM':E.matchedM, 'labels':E.labels}).toEqual(expected);
+        });
+        it('Egervary stepZero with input graph testset 2', function () {
+            var expected = {'matchedM':[-1,-1], 'labels':{1:2,2:3,3:0,4:0}};
+            var G = new Graph([1,2],[3,4], [[2,1],[3,0]]);
+            var E = new Egervary(G);
+            E.stepZero();
+            expect({'matchedM':E.matchedM, 'labels':E.labels}).toEqual(expected);
+        });
+        it('Egervary stepZero with input graph testset 3', function () {
+            var expected = {'matchedM':[-1,-1], 'labels':{'ken':2,2:3,3:0,'mary':0}};
+            var G = new Graph(['ken',2],[3,'mary'], [[2,1],[3,0]]);
+            var E = new Egervary(G);
+            E.stepZero();
+            expect({'matchedM':E.matchedM, 'labels':E.labels}).toEqual(expected);
+        });
+    });
+    describe('Egervary isRedEdge function test', function () {
+        it('Egervary isRedEdge without input graph', function () {
+            var expected = false;
+            var E = new Egervary();
+            E.stepZero();
+            expect(E.isRedEdge(0, 0)).toEqual(expected);
+        });
+        it('Egervary isRedEdge with input graph testset 1 v1', function () {
+            var expected = true;
+            var G = new Graph([1,2],[3,4], [[1,1],[1,0]]);
+            var E = new Egervary(G);
+            E.stepZero();
+            expect(E.isRedEdge(0, 0)).toEqual(expected);
+        });
+        it('Egervary isRedEdge with input graph testset 1 v2', function () {
+            var expected = false;
+            var G = new Graph([1,2],[3,4], [[1,1],[1,0]]);
+            var E = new Egervary(G);
+            E.stepZero();
+            expect(E.isRedEdge(1, 1)).toEqual(expected);
+        });
+        it('Egervary isRedEdge with input graph testset 2 v1', function () {
+            var expected = true;
+            var G = new Graph([1,2],[3,4], [[2,1],[3,0]]);
+            var E = new Egervary(G);
+            E.stepZero();
+            expect(E.isRedEdge(0, 0)).toEqual(expected);
+        });
+        it('Egervary isRedEdge with input graph testset 2 v2', function () {
+            var expected = false;
+            var G = new Graph([1,2],[3,4], [[2,1],[3,0]]);
+            var E = new Egervary(G);
+            E.stepZero();
+            expect(E.isRedEdge(1, 1)).toEqual(expected);
+        });
+    });
+    describe('Egervary initRedEdges function test', function () {
+        it('Egervary initRedEdges without input graph', function () {
+            var expected = [[]];
+            var E = new Egervary();
+            E.stepZero();
+            expect(E.redEdges).toEqual(expected);
+        });
+        it('Egervary initRedEdges with input graph testset 1', function () {
+            var expected = [[1,1],[1,0]];
+            var G = new Graph([1,2],[3,4], [[1,1],[1,0]]);
+            var E = new Egervary(G);
+            E.stepZero();
+            expect(E.redEdges).toEqual(expected);
+        });
+        it('Egervary initRedEdges with input graph testset 2', function () {
+            var expected = [[2,0],[3,0]];
+            var G = new Graph([1,2],[3,4], [[2,1],[3,0]]);
+            var E = new Egervary(G);
+            E.stepZero();
+            expect(E.redEdges).toEqual(expected);
+        });
+    });
+    describe('Egervary createPair function test', function () {
+        it('Egervary createPair with input graph testset 1 v1', function () {
+            var expected = [0,-1];
+            var G = new Graph([1,2],[3,4], [[1,1],[1,0]]);
+            var E = new Egervary(G);
+            E.stepZero();
+            E.createPair(0,0);
+            expect(E.matchedM).toEqual(expected);
+        });
+        it('Egervary createPair with input graph testset 1 v1', function () {
+            var expected = [0,1];
+            var G = new Graph([1,2],[3,4], [[1,1],[1,0]]);
+            var E = new Egervary(G);
+            E.stepZero();
+            E.createPair(0,0);
+            E.createPair(1,1);
+            expect(E.matchedM).toEqual(expected);
+        });
+    });
+    describe('Egervary isMatchedMFull function test', function () {
+        it('Egervary isMatchedMFull without input graph', function () {
+            var E = new Egervary();
+            E.stepZero();
+            expect(E.isMatchedMFull()).toEqual(true);
+        });
+        it('Egervary isMatchedMFull with input graph testset 1', function () {
+            var G = new Graph([1,2],[3,4], [[1,1],[1,0]]);
+            var E = new Egervary(G);
+            E.stepZero();
+            expect(E.isMatchedMFull()).toEqual(false);
+        });
+        it('Egervary isMatchedMFull with input graph testset 1 and pair creation', function () {
+            var G = new Graph([1,2],[3,4], [[1,1],[1,0]]);
+            var E = new Egervary(G);
+            E.stepZero();
+            E.createPair(0,0);
+            E.createPair(1,1);
+            expect(E.isMatchedMFull()).toEqual(true);
+        });
+        it('Egervary isMatchedMFull with input graph testset 2', function () {
+            var G = new Graph([1,2],[3,4], [[2,1],[3,0]]);
+            var E = new Egervary(G);
+            E.stepZero();
+            expect(E.isMatchedMFull()).toEqual(false);
+        });
+    });
+    describe('Egervary findFirstSingle function test', function () {
+        it('Egervary findFirstSingle with input graph testset 1', function () {
+            var expected = '0';
+            var G = new Graph([1,2],[3,4], [[1,1],[1,0]]);
+            var E = new Egervary(G);
+            E.stepZero();
+            expect(E.findFirstSingle()).toEqual(expected);
+        });
+        it('Egervary findFirstSingle with input graph testset 1 and pair creation', function () {
+            var expected = '1';
+            var G = new Graph([1,2],[3,4], [[1,1],[1,0]]);
+            var E = new Egervary(G);
+            E.stepZero();
+            E.createPair(0,0);
+            expect(E.findFirstSingle()).toEqual(expected);
+        });
+        it('Egervary findFirstSingle with input graph testset 1 and full pair creation', function () {
+            var expected = '-1';
+            var G = new Graph([1,2],[3,4], [[1,1],[1,0]]);
+            var E = new Egervary(G);
+            E.stepZero();
+            E.createPair(0,0);
+            E.createPair(1,1);
+            expect(E.findFirstSingle()).toEqual(expected);
+        });
+    });
+    describe('Egervary stepOne function test', function () {
+        it('Egervary stepOne without input graph', function () {
+            var expected = [];
+            var E = new Egervary();
+            E.stepZero();
+            E.stepOne();
+            expect(E.matchedM).toEqual(expected);
+        });
+    });
+});
